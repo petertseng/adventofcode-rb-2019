4bd99a22add8c73b13b907c104ea0cf1f8699572
Peter Tseng <pht24@cornell.edu>
2019-12-09 09:19:25 +0000

    intcode: Add some stats

--- 09_intcode_relative.rb
+++ 09_intcode_relative.rb
@@ -1,9 +1,10 @@
 require_relative 'lib/intcode'
 
 OPT = !ARGV.delete('--no-opt')
-SPARSE = ARGV.delete('-s')
+SPARSE = ARGV.delete('-sp')
 DISAS_DYNAMIC = ARGV.delete('-dd')
 disas_static = ARGV.delete('-ds')
+stats = ARGV.delete('-s')
 
 def run(mem, input)
   Intcode.new(mem, sparse: SPARSE, funopt: OPT).continue(disas: DISAS_DYNAMIC, input: input).output
@@ -15,3 +16,10 @@ puts run(mem, 1)
 puts run(mem, 2)
 
 Intcode.disas(mem) if disas_static
+
+if stats
+  ic = Intcode.new(mem)
+  ic.continue(stats: true, input: 2)
+  p ic.times_run.sort_by(&:last)
+  p ic.jumps_taken.sort_by(&:last)
+end
--- plain_intcode.rb
+++ plain_intcode.rb
@@ -3,11 +3,12 @@ require_relative 'lib/intcode'
 DISAS_DYNAMIC = ARGV.delete('-dd')
 disas_static = ARGV.delete('-ds')
 MEM = ARGV.delete('-m')
+STATS = ARGV.delete('-s')
 
 def run(mem, inputs)
   mem = mem.dup
   inputs = inputs.dup
-  Intcode.new(mem).continue(disas: DISAS_DYNAMIC, mem_all: MEM, input: -> { inputs.shift })
+  Intcode.new(mem).continue(stats: STATS, disas: DISAS_DYNAMIC, mem_all: MEM, input: -> { inputs.shift })
 end
 
 mem = ARGV.shift.split(?,).map(&method(:Integer)).freeze
@@ -15,5 +16,9 @@ inputs = ARGV.map(&method(:Integer))
 
 ic = run(mem, inputs)
 Intcode.disas(ic.mem) if disas_static
+if STATS
+  p ic.times_run.sort_by(&:last)
+  p ic.jumps_taken.sort_by(&:last)
+end
 puts "%0 #{ic.memory[0]}"
 puts "out #{ic.output}"

16a44d63bed3fc2b867800ee2476fa8f3a24d17b
Peter Tseng <pht24@cornell.edu>
2019-12-25 10:49:59 +0000

    intcode: add function call optimisation and logging

--- 09_intcode_relative.rb
+++ 09_intcode_relative.rb
@@ -1,10 +1,11 @@
 require_relative 'lib/intcode'
 
+OPT = !ARGV.delete('--no-opt')
 SPARSE = ARGV.delete('-s')
 DISAS_DYNAMIC = ARGV.delete('-dd')
 
 def run(mem, input)
-  Intcode.new(mem, sparse: SPARSE).continue(disas: DISAS_DYNAMIC, input: input).output
+  Intcode.new(mem, sparse: SPARSE, funopt: OPT).continue(disas: DISAS_DYNAMIC, input: input).output
 end
 
 mem = (ARGV[0]&.include?(?,) ? ARGV[0] : ARGF.read).split(?,).map(&method(:Integer)).freeze

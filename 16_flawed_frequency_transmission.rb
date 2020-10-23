2d553c2d663305c194d2c56b977601421b71fe4e
Peter Tseng <pht24@cornell.edu>
2020-10-23 11:39:31 +0000

    day 16: skip unnecessary digits
    cuts runtime to about 0.7x

--- 16_flawed_frequency_transmission.rb
+++ 16_flawed_frequency_transmission.rb
@@ -42,66 +42,62 @@ raise "Can't do it the fast way" unless offset * 2 >= input.size * 10000
 # Because binom(n, k) == binom(n, n - k), that's equal to:
 # binom(99 + i, 99) % 10.
 
-# Calculates binom(99 + i, 99) % 10.
-def binom_99_mod_10(i)
-  # By Chinese Remainder Theorem...
-  #
-  # x congruent to a_i mod n_i
-  # x = sum a_i y_i z_i
-  # y_i is the product of all other moduli
-  # z_i is the modular inverse of y_i mod n_i
-  #
-  # Modular inverse of 5 mod 2 is 1
-  # Modular inverse of 2 mod 5 is 3
-  #
-  # 1 * 5 = 5
-  # 2 * 3 = 6
-  #
-  # So that tells us we want:
-  # (binom(99 + i, 99) % 2) * 5 + (binom(99 + i, 99) % 5) * 6
-  #
-  # By Lucas's Theorem, binom(n, k) % 2 depends on the base-2 expansion of n and k.
-  # It's only nonzero if all digits of the expansions paired together are nonzero,
-  # Therefore, is equal to (n & k == k ? 1 : 0).
-  #
-  # So (binom(99 + i, 99) % 2) * 5 is rewritten as:
-  # ((99 + i) & 99 == 99 ? 1 : 0) * 5, or:
-  mod_2_term = (99 + i) & 99 == 99 ? 5 : 0
-
-  # In the same way, binom(n, k) % 5 depends on the base-5 expansion of n and k.
-  # The base-5 expansion of 99 is 344_5.
-  # If any base-5 digit of 99 + i is less than the corresponding base-5 digit in 344_5,
-  # then that contributes binom(n, k) for n < k, which is always 0
-  # (e.g. 2 choose 4 == 0).
-  # So the only possibilities for last three base-5 digits of base-5 expansion for n must be 344_5 or 444_5.
-  # For 344_5, result is binom(3, 3) * binom(4, 4) * binom(4, 4) = 1 * 1 * 1 = 1.
-  # For 444_5, result is binom(4, 3) * binom(4, 4) * binom(4, 4) = 4 * 1 * 1 = 4.
-  # Multiply by 6 to get 6 and 24, mod by 10 to get 6 and 4.
-  #
-  # These cases happen when (99 + i) % 125 == 99 and (99 + i) % 125 == 124.
-  # Subtract 99 from both sides to get i % 125 == 0 and i % 125 == 25.
-  mod_5_term = case i % 125
-  when 0; 6
-  when 25; 4
-  else; 0
-  end
-
-  (mod_2_term + mod_5_term) % 10
-end
+# By Chinese Remainder Theorem...
+#
+# x congruent to a_i mod n_i
+# x = sum a_i y_i z_i
+# y_i is the product of all other moduli
+# z_i is the modular inverse of y_i mod n_i
+#
+# Modular inverse of 5 mod 2 is 1
+# Modular inverse of 2 mod 5 is 3
+#
+# 1 * 5 = 5
+# 2 * 3 = 6
+#
+# So that tells us we want:
+# (binom(99 + i, 99) % 2) * 5 + (binom(99 + i, 99) % 5) * 6
 
 big_size = input.size * 10000 - offset
 rsize = 8
 
 result = [0] * rsize
-big_size.times { |i|
-  bin = binom_99_mod_10(i)
-  # No point doing array reads/writes if they're going to be multiplied by zero.
-  # Saves a decent chunk of time since about 92.25% of the coefficients are zero...
-  next if bin == 0
 
-  dist_from_end = big_size - i
-  [dist_from_end, rsize].min.times { |j|
-    result[j] += input[(offset + i + j) % input.size] * bin
+stride = ->(big_stride, little_strides) {
+  ((0...big_size) % big_stride).each { |big_stride_base|
+    little_strides.each { |little_stride, coeff|
+      i = big_stride_base + little_stride
+
+      dist_from_end = big_size - i
+      [dist_from_end, rsize].min.times { |j|
+        result[j] += input[(offset + i + j) % input.size] * coeff
+      }
+    }
   }
 }
+
+# By Lucas's Theorem, binom(n, k) % m depends on the base-m expansion of n and k.
+# It's the product of the binomials for each pair of digits of n and k paired
+# So for each position, that digit of n must be >= that digit of k.
+#
+# For base 2:
+# n & k == k ? 1 : 0
+# So (binom(99 + i, 99) % 2) * 5 is rewritten as:
+# ((99 + i) & 99 == 99 ? 1 : 0) * 5.
+# Since 99 in base 2 is 1100011, we know that the lower seven bits of (99 + i) must be 0b11xxx11.
+# So subtract 99 and the lower 7 bits of i must be 0b00xxx00.
+# Notice the values increase by 4 each time and three bits means 8 values.
+# Seven bits is 128.
+# Thus:
+# ((0...32) % 4).include?(i % 128) ? 5 : 0
+stride[128, ((0...32) % 4).to_h { |x| [x, 5] }]
+# For base 5:
+# The base-5 expansion of 99 is 344_5.
+# The only possibilities for last three base-5 digits of base-5 expansion for n must be 344_5 or 444_5.
+# For 344_5, result is binom(3, 3) * binom(4, 4) * binom(4, 4) = 1 * 1 * 1 = 1.
+# For 444_5, result is binom(4, 3) * binom(4, 4) * binom(4, 4) = 4 * 1 * 1 = 4.
+# Multiply by 6 to get 6 and 24, mod by 10 to get 6 and 4.
+# These cases happen when (99 + i) % 125 == 99 and (99 + i) % 125 == 124.
+# Subtract 99 from both sides to get i % 125 == 0 and i % 125 == 25.
+stride[125, {0 => 6, 25 => 4}]
 puts result.map { |x| x % 10 }.join
